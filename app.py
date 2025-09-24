dd8d538e83ec9a529affcbf100fb35e22e612e6a
Jensenix <jensenfinix@gmail.com>
2025-09-24 13:41:58 +0700

    add Procfile, requ, import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -99,4 +100,4 @@ def index():
                            shop=shop)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
